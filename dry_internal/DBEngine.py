847e3e574ef236f5e9eac3d3208a1b4361f23c58
Dieter <phodiale@ya.ru>
2024-03-28 19:06:14 +0300

    pg storage support

--- dry_internal/DBEngine.py
+++ dry_internal/DBEngine.py
@@ -1,4 +1,5 @@
 import sqlite3
+import psycopg2
 
 class DBEngine:
 	StorageSqlite = "sqlite"
@@ -18,6 +19,8 @@ class DBEngine:
 		pass
 	def writeGroupRecord(self, hash: str, fname: str, size: int):
 		pass
+	def close(self):
+		pass
 
 class PgPath:
 	def __init__(self) -> None:
@@ -29,20 +32,122 @@ class PgPath:
 		return bool(self.host) and bool(self.user) and bool(self.dbname)
 
 class PGEngine(DBEngine):
+	ProduccerId = None
+	SessionId = None
+
+	@staticmethod
+	def makeNodeKeys(produccer: int, session: int):
+		PGEngine.ProduccerId = produccer
+		PGEngine.SessionId = session
+
+
+	def __init__(self):
+		self.connection = None
+		self.cursor = None
+		self.path = ""
+
 	def open(self, path: PgPath):
-		pass
+		if not path.filled():
+			raise ValueError("Invalig pgconfig")
+		self.path = path
+		self.connection = psycopg2.connect(dbname=path.dbname, user=path.user, password=path.password, host=path.host, port="5432")
+		if not self.connection:
+			raise ConnectionError("cannot connect to %s:%s" % (path.host, path.dbname))
+		self.cursor = self.connection.cursor()
+		self.makeDb()
+	
+	def close(self):
+		self.cursor = None
+		if self.connection:
+			self.connection.close()
+		self.connection = None
+
+	def execOne(self, q, args):
+		self.checkDb()
+		try:
+			self.cursor.execute(q, args)
+		except Exception as e:
+			print(e, " in ", q)
+		
+
 	def checkDb(self):
-		pass
+		if not self.cursor or not self.connection:
+			raise Exception('dbEngine', 'not opened')
+		if not PGEngine.ProduccerId:
+			raise ValueError("Invalig ProduccerId")
+		if not PGEngine.SessionId:
+			raise ValueError("Invalig SessionId")
+
 	def makeDb(self):
-		pass
+		try:
+			print("use db user ",self.path.user)
+			self.cursor.execute("""
+				CREATE TABLE IF NOT EXISTS public.hashes
+				(
+					id bigserial NOT NULL,
+					producer_id integer NOT NULL,
+					session_id bigint NOT NULL,
+					path character varying(512) COLLATE pg_catalog."default" NOT NULL,
+					hash character varying(256) COLLATE pg_catalog."default" NOT NULL,
+					size bigint NOT NULL,
+					frags jsonb,
+					CONSTRAINT hashes_pkey PRIMARY KEY (id)
+				)
+			""")
+			self.cursor.execute("ALTER TABLE IF EXISTS public.hashes OWNER TO %s;", (self.path.user,))
+			self.cursor.execute("""
+				CREATE TABLE IF NOT EXISTS public.results
+				(
+					id bigserial NOT NULL,
+					session_id bigint NOT NULL,
+					"groupId" character varying(256) NOT NULL,
+					path character varying(512) NOT NULL,
+					size bigint NOT NULL,
+					PRIMARY KEY (id)
+				);
+			""")
+			self.cursor.execute("ALTER TABLE IF EXISTS public.results OWNER TO %s;", (self.path.user,))
+			self.cursor.execute("CREATE VIEW IF NOT EXISTS public.sessions AS SELECT DISTINCT session_id as sid FROM public.hashes order by session_id;")
+			self.cursor.execute("ALTER TABLE public.sessions OWNER TO %s;", (self.path.user,))
+
+		except Exception as e:
+			print(e, " in ",self.cursor.query)
+
+		self.connection.commit()
 	def notUniqueHashes(self):
-		pass
+		self.checkDb()
+		rc = []
+		self.execOne("SELECT DISTINCT hash FROM public.hashes WHERE session_id='%s' GROUP BY hash HAVING COUNT(*) > 1", (PGEngine.SessionId,))
+		try:
+			qrc = self.cursor.fetchall()
+		except psycopg2.ProgrammingError:
+			qrc = []
+		for row in qrc:
+			rc.append(row[0])
+		return rc
+
 	def filesByHash(self, hash: str):
-		pass
+		self.checkDb()
+		rc = []
+		self.execOne("SELECT path, size FROM public.hashes  WHERE hash='%s' and session_id='%s'", (hash,PGEngine.SessionId))
+		try:
+			qrc = self.cursor.fetchall()
+		except psycopg2.ProgrammingError:
+			qrc = []
+		for row in qrc:
+			rc.append((row[0], row[1]))
+		return rc
 	def writeFileInfo(self, path: str, hash: str, size: int):
-		pass
+		self.execOne("INSERT INTO public.hashes(producer_id, path, hash, size, session_id) VALUES (%s, %s, %s, %s, %s)", (PGEngine.ProduccerId, path, hash, size, PGEngine.SessionId))
+		self.connection.commit()
+		
 	def writeGroupRecord(self, hash: str, fname: str, size: int):
-		pass
+		self.execOne("INSERT INTO public.results(session_id, groupId, path, size) VALUES (%s, %s, %s, %s)", (PGEngine.SessionId, hash, fname, size))
+		self.connection.commit()
+
+	def cleanup(self):
+		self.execOne("DELETE FROM public.hashes WHERE session_id = %s", (PGEngine.SessionId,))
+		self.connection.commit()
 
 class SqliteEngine(DBEngine):
 	def __init__(self):
@@ -114,3 +219,4 @@ def makeEngine(type: str) -> DBEngine:
 	elif type.lower() == DBEngine.StorageSqlite:
 		return SqliteEngine()
 	raise ValueError("no %s storage" % type)
+
